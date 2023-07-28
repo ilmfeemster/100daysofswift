e2a3a176a3f73643111de58f92bb8c7e1a3f231e
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2023-07-28 10:45:42 -0700

    day 54

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
+    @IBOutlet var intensity2: UISlider!
+    @IBOutlet var intensity3: UISlider!
+    @IBOutlet var intensity4: UISlider!
+    @IBOutlet var sliderLabel: UILabel!
     var currentImage: UIImage!
     
     var context: CIContext!
-    var currentFilter: CIFilter!
+    var currentFilter: CIFilter!{
+        didSet{
+            sliderLabel.text = currentFilter.name
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +29,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         title = "YACIFP"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
         
+        sliderLabel.text = "CISepiaTone"
         context = CIContext()
         currentFilter = CIFilter(name: "CISepiaTone")
+        
     }
     
     @objc func importPicture() {
@@ -73,7 +83,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     }
     
     @IBAction func save(_ sender: Any) {
-        guard let image2 = imageView.image else {return}
+        guard let image2 = imageView.image else {return showError()}
         UIImageWriteToSavedPhotosAlbum(image2, self, #selector(image), nil)
     }
     
@@ -86,10 +96,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
 
         let inputKeys = currentFilter.inputKeys
 
-        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
-        if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+            intensity.isHidden = false
+        } else {
+            intensity.isHidden = true
+        }
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(intensity2.value * 200, forKey: kCIInputRadiusKey)
+            intensity.isHidden = false
+        } else {
+            intensity.isHidden = true
+        }
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(intensity3.value * 10, forKey: kCIInputScaleKey)
+            intensity.isHidden = false
+        } else {
+            intensity.isHidden = true
+        }
+        if inputKeys.contains(kCIInputCenterKey) {
+            currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey)
+            intensity.isHidden = false
+        } else {
+            intensity.isHidden = true
+        }
 
 
         
@@ -110,6 +140,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
             ac.addAction(UIAlertAction(title: "OK", style: .default))
             present(ac, animated: true)
         }
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Error", message: "Add an image to save.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Close", style: .cancel))
+        present(ac, animated: true)
     }
 }
 
