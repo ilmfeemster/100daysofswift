089dd69ff899c5c273de7c62e0dcbb3a9e7db949
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2023-08-10 05:08:47 -0700

    day 64

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Immanuel Matthews-Feemster on 8/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("got number \(i)")
+        }
+    }
+
+
+}
+
